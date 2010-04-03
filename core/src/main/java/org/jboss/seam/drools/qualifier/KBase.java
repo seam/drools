7fc4ae85169fb2099f03fd8f4d37eed942150922
Dan Allen <dan.j.allen@gmail.com>
2010-04-03 03:17:56 +0000

    add type and parameter targets

--- core/src/main/java/org/jboss/seam/drools/qualifier/KBase.java
+++ core/src/main/java/org/jboss/seam/drools/qualifier/KBase.java
@@ -2,6 +2,8 @@ package org.jboss.seam.drools.qualifier;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -12,7 +14,7 @@ import java.lang.annotation.Target;
 import javax.inject.Qualifier;
 
 @Qualifier
-@Target({METHOD, FIELD})
+@Target({TYPE, METHOD, FIELD, PARAMETER})
 @Documented
 @Retention(RUNTIME)
 @Inherited
